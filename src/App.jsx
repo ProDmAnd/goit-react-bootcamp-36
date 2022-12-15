b9291084f7015dd4d00f18d13e85534d7da9b6f9
Dmytro Prokhorenko <dm.an.prokhorenko@gmail.com>
2022-12-15 13:12:04 +0200

    Uncontrolled form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Counter, Wrapper } from 'components';
+import UncontrolledForm from 'components/UncontrolledForm/UncontrolledForm';
 
 export const App = () => {
   return (
     <Wrapper>
-      <Counter />
-      <Counter initialValue={10} increment={10} />
+      {/* <Counter /> */}
+      {/* <Counter initialValue={10} increment={10} /> */}
+      <UncontrolledForm />
     </Wrapper>
   );
 };
