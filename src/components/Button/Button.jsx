cec3f842853f3dd46c10fd892cd1c19fe8bd9c2b
Dmytro Prokhorenko <dm.an.prokhorenko@gmail.com>
2022-12-15 13:09:28 +0200

    Added disabled button styles, removed extra props. Added props spreading to tag

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,26 +1,19 @@
 import css from './Button.module.css';
 import PropTypes from 'prop-types';
 
-const Button = ({ children, clicked, success, error, onClick }) => {
-  const handleClick = () => {
-    const newClicked = clicked + 1;
-    onClick(newClicked);
-  };
-
-  console.log('rerender', clicked);
+const Button = ({ success, error, ...restProps }) => {
   const className = success ? css.green : error ? css.red : css.button;
   return (
-    <button onClick={handleClick} className={className}>
-      {children}
-    </button>
+    <button
+      {...restProps}
+      className={[className, restProps.className].join(' ')}
+    />
   );
 };
 
 Button.propTypes = {
   success: PropTypes.bool,
   error: PropTypes.bool,
-  onClick: PropTypes.func.isRequired,
-  clicked: PropTypes.number.isRequired,
 };
 
 export default Button;
