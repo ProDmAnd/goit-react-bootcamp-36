5ba359cf595f35d60fa420b83ca0dca04a219291
ProDmAnd <120254373+ProDmAnd@users.noreply.github.com>
2023-01-11 10:21:32 +0000

    Deploying to gh-pages from @ ProDmAnd/goit-react-bootcamp-36@8eb5298559eeb1c5b16a0a77db28eccf06055a2d 🚀

---
+++ static/js/not-found.1a209be1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[942],{9256:function(e,t,n){n.r(t);var r=n(184);t.default=function(){return(0,r.jsx)("div",{children:(0,r.jsx)("h2",{children:"Page not found"})})}}}]);
+//# sourceMappingURL=not-found.1a209be1.chunk.js.map
